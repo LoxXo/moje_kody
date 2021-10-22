4b3c0824acfeb74e40c3b74df681473e8ab1e68b
LoxXo <kubawa19@gmail.com>
2021-10-22 22:38:14 +0200

    sssdeee

--- pc_spec.py
+++ pc_spec.py
@@ -26,64 +26,135 @@
 # dodac TDP dla sprzetu i zrobic zliczanie łącznego zużycia ()
 # enum 
 
+from dataclasses import dataclass
+from typing import List, Optional
+
+
 class Power:
     power_TDP: int
 
 class Component:
-    is_connected: bool = False
+    __is_connected: bool = False
 
     def connect (self):
-        if self.is_connected == True:
-            raise Exception("Component is already connected")
-        self.is_connected = True
+        if self.__is_connected == True:
+            raise RuntimeError("Component is already connected")
+        self.__is_connected = True
         return self
 
     def disconnect (self):
-        if self.is_connected == False:
-            raise Exception("Component is disconnected")
-        self.is_connected = False
+        if self.__is_connected == False:
+            raise RuntimeError("Component is disconnected")
+        self.__is_connected = False
         return self
 
-class Motherboard:
-    __brand: str
-    __cpu_socket: str
-    __ram_slots = int
-    __ram_max_frequency = int
+class PCIe:
+    __bank_nr: List[int]
 
 
-class Intel_Motherboard(Motherboard):
-    pass
-
-class AMD_Motherboard(Motherboard):
-    pass
-
-class CPU:
-    __clock: float
+class CPU(Component, Power):
     __gen_socket: str
+    __cores: int
+    __clock_GHz: float
 
-class Intel_CPU(CPU):
+    def __init__(self, socket: str, cores: int, clock: float):
+            self.__gen_socket = socket
+            self.__cores = cores
+            self.__clock_GHz = clock
+
+    def __repr__(self):
+            return f'{self.__gen_socket}, {self.__cores} cores, {self.__clock_GHz} GHz'
+
+    #getter?
+    @property
+    def gen_socket(self) -> str:
+        return self.__gen_socket
+
+# class Intel_CPU(CPU):
     
-    pass
+#     pass
 
-class AMD_CPU(CPU):
+# class AMD_CPU(CPU):
     
-    pass
+#     pass
 
-class Memory:
-    bank_nr: int[1,2,3,4]
+class Memory(Component, Power):
+    __sizeGB: int
+    __stock_frequency: int
+    __max_frequency: int ## wynika z plyty glownej
 
+    @property
+    def mem_size(self):
+        return self.__sizeGB
 
-class Hard_drive:
-    sizeingb = float
+    @property
+    def frequency(self):
+        return self.__max_frequency
 
+    @frequency.setter ## jest chujowo bo daje najwyzsza wartosc zawsze niezaleznie od zalenosci
+    def frequency(self, mobo_freq: int):
+        self.__max_frequency = min(self.__stock_frequency, mobo_freq)
 
-class Pcie:
-    pass
+    def __init__(self, sizeGB: int, stock_freq: int):
+        self.__sizeGB = sizeGB
+        self.__stock_frequency = stock_freq
+
+    def __repr__(self):
+        return f'{self.__sizeGB} GB, {self.__stock_frequency} stock MHz, {self.__max_frequency} max MHz'
+class Hard_drive(Component, Power):
+    __sizeinGB = float
+    __read_speed = float
+    __write_speed = float
 
 class GPU:
-    clock: float
-    memoryinGB: float
+    __clock: float
+    __memoryinGB: float
 
+@dataclass
+class Motherboard(PCIe, Component, Power):
+    __brand: str
+    __cpu_socket: str
+    __ram_slots: List[Optional[Memory]]
+    __ram_max_frequency: int
 
-class PC(Motherboard, CPU, Memory, Hard_drive, Pcie, GPU):
-    running = False
+    def __init__(self, brand: str, cpu_socket: str, ram_slots: int, ram_max_freqeuncy: int):
+        self.__brand = brand
+        self.__cpu_socket = cpu_socket
+        self.__ram_slots = [None]*ram_slots
+        self.__ram_max_frequency = ram_max_freqeuncy
+        
+
+    def insert_RAM(self, ram: Memory, slot: int):
+        if len(self.__ram_slots) < slot:
+            raise RuntimeError("Nie mozesz wsadzic do tego slota, on nie istnieje")
+        if self.__ram_slots[slot] is not None:
+            raise RuntimeError("Slot jest zajety")
+        
+        ram.frequency = self.__ram_max_frequency
+        self.__ram_slots[slot] = ram
+        ram.connect()
+
+    @property
+    def memory_size(self):
+        return sum([mem.mem_size for mem in self.__ram_slots if mem is not None])
+        # for  in self.__ram_slots:
+        #     if self.__ram_slots[] == None
+        
+# class Intel_Motherboard(Motherboard):
+
+#     pass
+
+# class AMD_Motherboard(Motherboard):
+#     pass
+
+class PC(Motherboard, CPU, Memory, Hard_drive, PCIe, GPU):
+    __running = False
+
+if __name__ == "__main__":
+    ram1 = Memory(8, 2666)
+    ram2 = Memory(8, 2666)
+    mobo = Motherboard("Gowno", "ll?", 4, 3200)
+    mobo.insert_RAM(ram1, 1)
+    mobo.insert_RAM(ram2, 2)
+
+    print(mobo)
